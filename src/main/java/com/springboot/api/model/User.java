ae4d42ca460b421d8ffed66b82fb5c13f3223cd0
marlonfurtado <marlonfurtado.rs@gmail.com>
2017-11-11 21:55:39 -0200

    Add user model

---
+++ src/main/java/com/springboot/api/model/User.java
@@ -0,0 +1,69 @@
+package com.springboot.api.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class User {
+
+	private Integer id;
+	
+	@JsonProperty("first_name")
+	private String firstName;
+
+	@JsonProperty("last_name")
+	private String lastName;
+	
+	private String username;
+	private String password;
+	private String email;
+	private String birthday;
+
+
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getBirthday() {
+		return birthday;
+	}
+	public void setBirthday(String birthday) {
+		this.birthday = birthday;
+	}
+	
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", username=" + username
+				+ ", password=" + password + ", email=" + email + ", birthday=" + birthday + "]";
+	}
+}
